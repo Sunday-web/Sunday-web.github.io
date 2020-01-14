e2b2da10b192e704fd003fb36261ed37d6b25d9b
sunday <775011185@qq.com>
2020-01-14 11:33:57 +0800

    新增雷达图

--- echart/js/chart.js
+++ echart/js/chart.js
@@ -2903,6 +2903,205 @@ $(function () {
         //图表显示
         myChart.setOption(option)
     })();
+    //仪表盘-3
+    !(function () {
+        // 初始化实例
+        var gauge_val = {
+            name: "市一医院周边\n泊位指数",
+            value: 50.26
+        };
+        if (gauge_val.value > 1) {
+            var color_pct = parseFloat((gauge_val.value / 100).toFixed(2))
+        } else {
+            var color_pct = parseFloat(gauge_val.value)
+        }
+        var myChart = echarts.init(document.getElementById('gauge-3'));
+                var option = {
+            // backgroundColor: '#043654',
+            series: [{
+                    name: '刻度',
+                    type: 'gauge',
+                    radius: '95%',
+                    min: 0,
+                    max: 100,
+                    splitNumber: 5, //刻度数量
+                    startAngle: 0,
+                    endAngle: 180,
+                    axisLine: {
+                        show: true,
+                        lineStyle: {
+                            width: 1,
+                            color: [
+                                [1, 'rgba(0,0,0,0)']
+                            ]
+                        }
+                    }, //仪表盘轴线
+                    axisLabel: {
+                        show: false,
+                        color: '#fff',
+                        distance: 10
+                    }, //刻度标签。
+                    axisTick: {
+                        show: true,
+                        splitNumber: 10,
+                        lineStyle: {
+                            color: '#09C2FF',
+                            width: 1
+                        },
+                        length: -1
+                    }, //刻度样式
+                    splitLine: {
+                        show: true,
+                        length: -1,
+                        lineStyle: {
+                            color: '#09C2FF',
+                            width: 1
+                        }
+                    }, //分隔线样式
+                    detail: {
+                        show: false
+                    },
+                    pointer: {
+                        show: false
+                    }
+                },
+                {
+                    type: 'gauge',
+                    radius: '85%',
+                    center: ['50%', '50%'],
+                    splitNumber: 0, //刻度数量
+                    startAngle: 180,
+                    endAngle: 0,
+                    axisLine: {
+                        show: true,
+                        lineStyle: {
+                            width: 10,
+                            color: [
+                                [
+                                    color_pct, new echarts.graphic.LinearGradient(
+                                        0, 0, 1, 0, [{
+                                                offset: 0,
+                                                color: 'rgba(59,150,241,1)'
+                                            },
+                                            {
+                                                offset: 1,
+                                                color: 'rgba(59,150,241,0.2)'
+                                            }
+                                        ]
+                                    )
+                                ],
+                                [
+                                    1, 'rgba(65,62,84,0)'
+                                ]
+                            ]
+                        }
+                    },
+                    //分隔线样式。
+                    splitLine: {
+                        show: false,
+                    },
+                    axisLabel: {
+                        show: false
+                    },
+                    axisTick: {
+                        show: false
+                    },
+                    pointer: {
+                        show: false
+                    },
+                    title: {
+                        show: false,
+                        offsetCenter: [0, '-26%'], // x, y，单位px
+                        textStyle: {
+                            color: '#fff',
+                            fontSize: 14
+                        }
+                    },
+                    //仪表盘详情，用于显示数据。
+                    detail: {
+                        show: false,
+                        offsetCenter: [0, '16%'],
+                        color: '#ffffff',
+                        formatter: function (params) {
+                            return params
+                        },
+                        textStyle: {
+                            fontSize: 44
+                        }
+                    },
+                    data: [{
+                        value: 100
+                    }]
+                },
+                {
+                    type: 'gauge',
+                    radius: '60%',
+                    center: ['50%', '50%'],
+                    splitNumber: 0, //刻度数量
+                    startAngle: 180,
+                    endAngle: 0,
+                    axisLine: {
+                        show: true,
+                        lineStyle: {
+                            width: 150,
+                            color: [
+                                [
+                                    1, new echarts.graphic.LinearGradient(
+                                        0, 0, 1, 0, [{
+                                                offset: 0,
+                                                color: '#012242'
+                                            },
+                                            {
+                                                offset: 1,
+                                                color: '#01376D'
+                                            }
+                                        ]
+                                    )
+                                ]
+                            ]
+                        }
+                    },
+                    //分隔线样式。
+                    splitLine: {
+                        show: false,
+                    },
+                    axisLabel: {
+                        show: false
+                    },
+                    axisTick: {
+                        show: false
+                    },
+                    pointer: {
+                        show: false
+                    },
+                    title: {
+                        show: true,
+                        offsetCenter: [0, getSize(0.5)], // x, y，单位px
+                        textStyle: {
+                            color: '#fff',
+                            fontSize: getSize(0.24),
+                            lineHeight: getSize(0.35)
+                        }
+                    },
+                    //仪表盘详情，用于显示数据。
+                    detail: {
+                        show: true,
+                        offsetCenter: [0, getSize(-0.2)],
+                        color: '#00FCFF',
+                        formatter: function (params) {
+                            return params
+                        },
+                        textStyle: {
+                            fontSize: getSize(0.3)
+                        }
+                    },
+                    data: [gauge_val]
+                }
+            ]
+        };
+        //图表显示
+        myChart.setOption(option)
+    })();
 
     //水滴图
     !(function () {
