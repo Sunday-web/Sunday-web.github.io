1673509c498dd24fe16a49547810f8d1dc071bd1
Sunday <775011185@qq.com>
2025-03-13 09:43:16 +0800

    2025061

--- 3d/data.js
+++ 3d/data.js
@@ -9,6 +9,62 @@ var DATA = {
   result: [
     {
       name: '3D',
+      code: '2025061',
+      detailsLink: '/c/2025/03/12/608259.shtml',
+      videoLink: '',
+      date: '2025-03-12(三)',
+      week: '三',
+      red: '4,7,6',
+      blue: '',
+      blue2: '',
+      sales: '139972702',
+      poolmoney: '',
+      content: '',
+      addmoney: '',
+      addmoney2: '',
+      msg: '',
+      z2add: '',
+      m2add: '',
+      prizegrades: [
+        {
+          type: 1,
+          typenum: '',
+          typemoney: '',
+        },
+        {
+          type: 2,
+          typenum: '',
+          typemoney: '',
+        },
+        {
+          type: 3,
+          typenum: '',
+          typemoney: '',
+        },
+        {
+          type: 4,
+          typenum: '',
+          typemoney: '',
+        },
+        {
+          type: 5,
+          typenum: '',
+          typemoney: '',
+        },
+        {
+          type: 6,
+          typenum: '',
+          typemoney: '',
+        },
+        {
+          type: 7,
+          typenum: '',
+          typemoney: '',
+        },
+      ],
+    },
+    {
+      name: '3D',
       code: '2025060',
       detailsLink: '/c/2025/03/11/608052.shtml',
       videoLink: '',
